be867ec363bfd23bcddd5de953ef4652e945f28e
김수환 <zzanggoon8@hiconsy.com>
2023-03-09 12:36:46 +0900

    first commit

---
+++ src/main/java/me/ssu/refactoringrestapi/RefactoringRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.refactoringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssu/refactoringrestapi/RefactoringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.refactoringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
